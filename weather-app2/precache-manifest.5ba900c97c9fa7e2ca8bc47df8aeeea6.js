10e45c0b491feb517934708d107bed52277adbe1
Dan <dan@mail.com>
2020-05-03 14:33:42 +0200

    auto-commit

---
+++ weather-app2/precache-manifest.5ba900c97c9fa7e2ca8bc47df8aeeea6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6460059f34d6c87b33470b9df4400451",
+    "url": "/weather-app2/index.html"
+  },
+  {
+    "revision": "f3b8f43338608404831f",
+    "url": "/weather-app2/static/css/main.eda5152c.chunk.css"
+  },
+  {
+    "revision": "a2c9a8ab658ba4def3c6",
+    "url": "/weather-app2/static/js/2.21aa083e.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/weather-app2/static/js/2.21aa083e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3b8f43338608404831f",
+    "url": "/weather-app2/static/js/main.9c1fdf24.chunk.js"
+  },
+  {
+    "revision": "7c8ce27cbae676af463a",
+    "url": "/weather-app2/static/js/runtime-main.a754bfe1.js"
+  },
+  {
+    "revision": "9eb509e0193397bb5d7977a2b3a526aa",
+    "url": "/weather-app2/static/media/vejr.9eb509e0.jpg"
+  }
+]);
